150e368694ed2a77a925d8af5ef9d4576484700b
Ivan Sebastian <ivansedbert@gmail.com>
2019-05-20 20:21:31 +0700

    spriteKit

--- Relaxace Nano Challenge/ViewController.swift
+++ Relaxace Nano Challenge/ViewController.swift
@@ -7,12 +7,31 @@
 //
 
 import UIKit
+import SpriteKit
+
 
 class ViewController: UIViewController {
 
+    @IBOutlet var teste: UIView!
+    let sk:SKView = SKView()
+   
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    sk.frame = teste.bounds
+        sk.backgroundColor = .black
+        teste.addSubview(sk)
+        
+         let scene:SKScene = SKScene(size: teste.bounds.size)
+        scene.scaleMode = .aspectFit
+        scene.backgroundColor = .clear
+        
+        let en = SKEmitterNode(fileNamed: "MyParticle")
+        en? .position = sk.center
+        
+        scene.addChild(en!)
+        sk.presentScene(scene)
+        
     }
 
 
